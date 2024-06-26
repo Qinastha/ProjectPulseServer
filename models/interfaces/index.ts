8ffa2c545ed5fc3873eea804fdc585cf6527f265
Anton <nanderson@banjohealth.com>
2024-06-26 19:42:21 +0200

    update Profile schema

--- models/interfaces/index.ts
+++ models/interfaces/index.ts
@@ -1,1 +1,2 @@
 export * from './taskChecklistItem.interface';
+export * from './userAddress.interface'
--- models/schemas/Profile.schema.ts
+++ models/schemas/Profile.schema.ts
@@ -1,13 +1,23 @@
 import mongoose, {Document, Schema} from 'mongoose';
+import {UserAddress} from "../interfaces";
 
 interface IProfile extends Document {
     avatar: string;
-    language: string;
+    phoneNumber: string,
+    gender: string,
+    address: UserAddress,
+    language: string,
+    timeZone: string,
+
 }
 
 const profileSchema = new Schema<IProfile>({
     avatar: {type: String},
-    language: {type:String,required: true, default: 'eng'},
+    phoneNumber: {type: String,unique: true,required: true},
+    gender: {type: String,required: true},
+    address: {any: Object},
+    language: {type: String},
+    timeZone: {type: String,required: true}
 });
 
 export const Profile = mongoose.model<IProfile>('Profile', profileSchema);
