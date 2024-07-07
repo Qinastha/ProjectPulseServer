fe9b8a586cb0ae81fd4a16b5f23d9416d1116fb0
Anton <nanderson@banjohealth.com>
2024-07-07 23:15:08 +0200

    Update user.routes.ts

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import {deleteUser, getAllUsers, getUser, updateUser} from "../controllers";
-import {checkAuth, checkAdminRole} from "../middlewares";
+import {checkAuth} from "../middlewares";
 
 const router = express.Router();
 
 router.get('/',checkAuth,getUser)
-router.get('/all',checkAuth,checkAdminRole,getAllUsers)
+router.get('/all',checkAuth,getAllUsers)
 router.put('/update',checkAuth,updateUser)
 router.delete('/delete',checkAuth,deleteUser)
 
