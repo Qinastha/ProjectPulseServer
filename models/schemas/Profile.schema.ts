27bbd1c27997b516a6433451efe98439e63135f0
Anton <nanderson@banjohealth.com>
2024-07-30 20:00:22 +0200

    Update Profile.schema.ts

--- models/schemas/Profile.schema.ts
+++ models/schemas/Profile.schema.ts
@@ -8,7 +8,7 @@ interface IProfile extends Document {
     address: UserAddress,
     language: string,
     timeZone: string,
-
+    widgets: string[]
 }
 
 const profileSchema = new Schema<IProfile>({
@@ -17,7 +17,8 @@ const profileSchema = new Schema<IProfile>({
     gender: {type: String,required: true},
     address: {type: Schema.Types.Mixed},
     language: {type: String},
-    timeZone: {type: String,required: true}
+    timeZone: {type: String,required: true},
+    widgets: { type: [String],required: true,default: ['numberOfProjects','numberOfUsers','projectMembersPerformance']}
 });
 
 export const Profile = mongoose.model<IProfile>('Profile', profileSchema);
