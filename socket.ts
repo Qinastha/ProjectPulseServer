d3c62dd0f98430e1849fcd5c4f19d54ce81cb125
Qinastha <146641022+Qinastha@users.noreply.github.com>
2024-08-28 16:40:11 +0300

    Update error handling for sockets

--- socket.ts
+++ socket.ts
@@ -9,45 +9,58 @@ export default (io:any) => {
         });
 
         socket.on('sendMessage', async (data: Partial<SocketData>) => {
-            if (data.chatId && data.message) {
-            const { chatId, message } = data;
+            try {
+                if (data.chatId && data.message) {
+                    const { chatId, message } = data;
 
-            const newMessage = await createMessage(message)
-            if (newMessage) {
-                socket.emit('message', newMessage)
-                socket.broadcast.to(chatId).emit('message', newMessage)
-            }}
+                    const newMessage = await createMessage(message);
+                    if (newMessage) {
+                        socket.emit('message', newMessage);
+                        socket.broadcast.to(chatId).emit('message', newMessage);
+                    }
+                }
+            } catch (error: any) {
+                socket.emit('error', { message: 'Socket Error', details: error.message }, 500, false);
+            }
         });
 
         socket.on('messageTyping', (data: Partial<SocketData>) => {
             if (data.chatId && data.sender) {
-                const {chatId, sender} = data;
+                const { chatId, sender } = data;
                 socket.broadcast.to(chatId).emit('typingMessage', sender);
             }
         });
 
         socket.on('messageStopTyping', (data: Partial<SocketData>) => {
             if (data.chatId && data.sender) {
-                const {chatId, sender} = data;
+                const { chatId, sender } = data;
                 socket.broadcast.to(chatId).emit('stopTypingMessage', sender);
             }
         });
 
         socket.on('editMessage', async (data: Partial<SocketData>) => {
-            if (data.chatId && data.messageId && data.content) {
-                const {chatId, messageId, content} = data;
-                const updatedMessage = await updateMessage(messageId, content)
-                socket.broadcast.to(chatId).emit('messageEdit', updatedMessage);
+            try {
+                if (data.chatId && data.messageId && data.content) {
+                    const { chatId, messageId, content } = data;
+                    const updatedMessage = await updateMessage(messageId, content);
+                    socket.broadcast.to(chatId).emit('messageEdit', updatedMessage);
+                }
+            } catch (error: any) {
+                socket.emit('error', { message: 'Socket Error', details: error.message }, 500, false);
             }
-        })
+        });
 
         socket.on('deleteMessage', async (data: Partial<SocketData>) => {
-            if (data.chatId && data.messageId) {
-                const {chatId, messageId} = data;
-                const deletedMessageId = await deleteMessage(chatId, messageId)
-                socket.broadcast.to(chatId).emit('messageDelete', deletedMessageId);
+            try {
+                if (data.chatId && data.messageId) {
+                    const { chatId, messageId } = data;
+                    const deletedMessageId = await deleteMessage(chatId, messageId);
+                    socket.broadcast.to(chatId).emit('messageDelete', deletedMessageId);
+                }
+            } catch (error: any) {
+                socket.emit('error', { message: 'Socket Error', details: error.message }, 500, false);
             }
-        })
+        });
 
         socket.on('disconnect', () => {
             console.log('Client disconnected:', socket.id);
